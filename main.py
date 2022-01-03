2648441aa46ee60af90036fedd49ca47e44e6334
onepierre <clarou.p@free.fr>
2022-01-03 16:27:00 +0100

    Main"

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ import torch
 import torch.nn.functional as F
 import torch.utils.data as data
 import tqdm
+import sys
 import numpy as np
 import json
+import nltk
+from nltk.corpus import stopwords
 
-from collections import Counter
 from dataloader import SQuAD
+from evaluate import scores
+
+from models.naivefilter import basicFilter
 
 if __name__ == "__main__":
 
     test = SQuAD()
     test.loadSquad("Squad/train-v1.1.json","Squad/dev-v1.1.json")
 
-    print(test.questions_train[50])
-    print(test.contexts[10])
+    model1 = basicFilter(test.contexts)
+    model1.loadQuestions(test.questions_train)
+    print("Loaded")
+    model1.computeModel()
