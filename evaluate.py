2ef8551f6c3c1f6f9bb7fa62bfbe76b7aa00e8b0
onepierre <clarou.p@free.fr>
2022-01-03 16:28:15 +0100

    Print labels for debug

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,6 @@
-from sklearn.metrics import F1_score,accuracy_score
+from sklearn.metrics import f1_score,accuracy_score
 
 # Basics accuracy and F1
 def scores(y_true, y_pred):
-    reutrn (accuracy_score(y_true, y_pred) ,F1_score(y_true, y_pred))
+    print(y_true,y_pred)
+    return (accuracy_score(y_true, y_pred) ,f1_score(y_true, y_pred,average='micro'))
