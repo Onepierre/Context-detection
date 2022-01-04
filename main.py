9b7cc54b6d0792023678f26061e33b50faf48421
onepierre <clarou.p@free.fr>
2022-01-04 20:11:43 +0100

    main.py

--- main.py
+++ main.py
@@ -15,22 +15,25 @@ from evaluate import scores
 
 from models.naivefilter import BasicFilter
 from models.bertfilter import BertFilter
+from models.doc2vec import doc2VecFilter
 
 if __name__ == "__main__":
 
     test = SQuAD()
     test.loadSquad("Squad/train-v1.1.json","Squad/dev-v1.1.json")
 
-    # # Basic filter
-    # model1 = BasicFilter(test.contexts)
-    # model1.loadQuestions(test.questions_train)
-    # print("Loaded")
-    # model1.computeModel()
+    # Basic filter
+    model1 = BasicFilter(test.contexts)
+    model1.loadQuestions(test.questions_train[:1000])
+    print("Loaded")
+    model1.computeModel()
+    #model1.getConfidence()
 
     # BERT encodings comparison
-    model2 = BertFilter()
-    model2.encodeContexts(test.contexts)
-    print("Model loaded")
-    model2.encodeQuestions(test.questions_train)
-    print("Questions encoded")
-    model2.computeModel()
+    # model2 = BertFilter()
+    # model2 = doc2VecFilter()
+    # model2.encodeContexts(test.contexts[:100])
+    # print("Model loaded")
+    # model2.encodeQuestions(test.questions_train)
+    # print("Questions encoded")
+    # model2.computeModel()
