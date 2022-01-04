a763232d8eeec40276b4b329b949a366e47c8133
onepierre <clarou.p@free.fr>
2022-01-04 20:11:24 +0100

    scoring

--- evaluate.py
+++ evaluate.py
@@ -3,4 +3,4 @@ from sklearn.metrics import f1_score,accuracy_score
 # Basics accuracy and F1
 def scores(y_true, y_pred):
     print(y_true,y_pred)
-    return (accuracy_score(y_true, y_pred) ,f1_score(y_true, y_pred,average='micro'))
+    return (accuracy_score(y_true, y_pred) ,f1_score(y_true, y_pred,average='weighted'))
